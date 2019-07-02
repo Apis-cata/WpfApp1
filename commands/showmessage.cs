3e4506203dcfbfb678a3a9f90a9316d0f261a584
Ilves Dmitry <scriptdev@mail.ru>
2019-07-03 01:07:25 +0300

    загрузка в репо

---
+++ commands/showmessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Notifyicon.Commands
+{
+    /// <summary>
+    /// A simple command that displays the command parameter as
+    /// a dialog message.
+    /// </summary>
+    public class ShowMessageCommand : ICommand
+    {
+        public void Execute(object parameter)
+        {
+            MessageBox.Show(parameter.ToString());
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+
+
+}
